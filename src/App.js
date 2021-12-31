a6835c8a4746d3e01fe1123c5d91d5c0e95d537c
Laura Oberwetter <leoberwetter@gmail.com>
2021-12-31 14:24:17 -0600

    not working yet

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 import Home from "./Components/home/Home";
 import About from "./Components/about/About";
-import Menu from "./Components/menu/Menu";
-import Contact from "./Components/contact/Contact";
+import {Menu} from "./Components/menu/Menu";
+import {Contact} from "./Components/contact/Contact";
 
 
-function App() {
-  return (
-    //react wrapper
-    <React.Fragment> 
-      <Router>
-        <Link>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/about" component={About}/>
-          <Route exact path="/menu" component={Menu}/>
-          <Route exact path="/contact" component={Contact}/>
-          <Route component={404}/>
-        </Link>
-      </Router>
-
-    </React.Fragment>
-  );
+class App extends Component {
+  render () {
+    return (
+      <React.Fragment> 
+        <Router>
+          <Link>
+            <Route exact path="/" component={<Home />}/>
+            <Route exact path="/about" component={<About />}/>
+            <Route exact path="/menu" component={Menu}/>
+            <Route exact path="/contact" component={Contact}/>
+            <Route component={404}/>
+          </Link>
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
+
 
 export default App;
--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const Home = () => (
-    <div>
-        <h1>hello hello</h1>
-    </div>
-)
+class Home extends Component {
+    render () {
+        return (
+            <div>
+              <h4>plzzzzz</h4>
+            </div>
+          )
+        }
+    }
+  
+  export default Home
+
+// export const Home = () => (
+//     <div>
+//         <h1>hello hello</h1>
+//         <p>why isnt this working</p>
+//     </div>
+// )
 
 
 // import React, { Component } from 'react';
@@ -19,4 +32,3 @@ export const Home = () => (
 //     }
 // }
 
-export default Home;
