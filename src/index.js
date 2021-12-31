82a2349ebe0b23088398d8bbed35dd236a1a80ed
Laura Oberwetter <leoberwetter@gmail.com>
2021-12-31 12:28:50 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import 'bootstrap/dist/css/bootstrap.min.css'; // can be here or App.js
 
 ReactDOM.render(
   <React.StrictMode>
